f11ef1ab137a70a70c3054097792eb0c8a3aacce
Blaine Schmeisser <BlaineSch@gmail.com>
2012-09-20 20:09:43 -0500

    Remove the spaces and add tabs.
    How did my code get so dirty??

--- extensions/adapter/storage/cache/MemoizerProxy.php
+++ extensions/adapter/storage/cache/MemoizerProxy.php
@@ -9,69 +9,69 @@ namespace li3_memoize\extensions\adapter;
  */
 class MemoizerProxy extends \lithium\template\Helper {
 
-    /**
-     * This is where the results are stored.
-     */
-    protected $_memoizeResults = [];
+	/**
+	 * This is where the results are stored.
+	 */
+	protected $_memoizeResults = [];
 
-    /**
-     * A copy of the helper we are proxying
-     */
-    protected $helper;
+	/**
+	 * A copy of the helper we are proxying
+	 */
+	protected $helper;
 
-    /**
-     * A list of methods that need to be filtered
-     */
-    protected $methods = array();
+	/**
+	 * A list of methods that need to be filtered
+	 */
+	protected $methods = array();
 
-    /**
-     * The constructor will accept the helper and cache it.
-     * @param object $helper 
-     */
-    public function __construct($helper, &$methods) {
-        $this->helper = $helper;
-        $this->methods = $methods;
-    }
+	/**
+	 * The constructor will accept the helper and cache it.
+	 * @param object $helper 
+	 */
+	public function __construct($helper, &$methods) {
+		$this->helper = $helper;
+		$this->methods = $methods;
+	}
 
-    /**
-     * __call
-     *
-     * The heart of the application. The method and params go in, the result is then cached and servered next time.
-     *
-     * @param string $method
-     * @param array $params
-     * @return mixed
-     */
-    public function __call($method, $params) {
+	/**
+	 * __call
+	 *
+	 * The heart of the application. The method and params go in, the result is then cached and servered next time.
+	 *
+	 * @param string $method
+	 * @param array $params
+	 * @return mixed
+	 */
+	public function __call($method, $params) {
 
-        // Eval
-        $key = $this->_getHash($params);
+		// Eval
+		$key = $this->_getHash($params);
 
-        // Create array if it doesn't exist
-        if(!isset($this->_memoizeResults[$method])) {
-            $this->_memoizeResults[$method] = [];
-        }
+		// Create array if it doesn't exist
+		if(!isset($this->_memoizeResults[$method])) {
+			$this->_memoizeResults[$method] = [];
+		}
 
-        // Check if method + params have been ran already
-        if(isset($this->_memoizeResults[$method][$key])) {
-            return $this->_memoizeResults[$method][$key];
-        }
+		// Check if method + params have been ran already
+		if(isset($this->_memoizeResults[$method][$key])) {
+			return $this->_memoizeResults[$method][$key];
+		}
 
-        // Get results
-        return ($this->_memoizeResults[$method][$key] = call_user_func_array(array($this->helper, $method), $params));
-    }
+		// Get results
+		return ($this->_memoizeResults[$method][$key] = call_user_func_array(array($this->helper, $method), $params));
+	}
 
-    /**
-     * _getHash
-     *
-     * The current params go in, a unique hash comes out.
-     *
-     * This was put into it's own method so that it can be overwritten by your class if you'd like to improve/simplify it.
-     *
-     * @param mixed $data
-     * @return string
-     */
-    protected function _getHash($data) {
-        return md5(serialize(func_get_args()));
-    }
+	/**
+	 * _getHash
+	 *
+	 * The current params go in, a unique hash comes out.
+	 *
+	 * This was put into it's own method so that it can be overwritten by your class if you'd like to improve/simplify it.
+	 *
+	 * @param mixed $data
+	 * @return string
+	 */
+	protected function _getHash($data) {
+		return md5(serialize(func_get_args()));
+	}
 }
