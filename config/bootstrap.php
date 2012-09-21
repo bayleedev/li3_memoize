266c8664c4be21a71bb2557e0119685940f66338
Blaine Schmeisser <BlaineSch@gmail.com>
2012-09-20 20:15:04 -0500

    Add memoize use call to the bootstrap.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use lithium\util\collection\Filters;
+use li3_memoize\extensions\Memoize;
 
 // Filter helpers
 Filters::apply('lithium\core\Libraries', 'instance', function($self, $params, $chain) {
