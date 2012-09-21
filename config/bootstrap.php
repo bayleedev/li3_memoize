5a535705df12b81235be21361980824245d26e1d
Blaine Schmeisser <BlaineSch@gmail.com>
2012-09-20 20:08:34 -0500

    Start filtering helpers

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,7 +2,11 @@
 
 use lithium\util\collection\Filters;
 
+// Filter helpers
 Filters::apply('lithium\core\Libraries', 'instance', function($self, $params, $chain) {
 	$object = $chain->next($self, $params, $chain);
+	if($params['type'] == 'helper') {
+		$object = Memoize::instance('helper', $object);
+	}
 	return $object;
 });
