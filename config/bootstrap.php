1dc5ba5ae2756c1ae03c666022c46a3cf9ce099c
Blaine Schmeisser <BlaineSch@gmail.com>
2012-09-24 00:18:41 -0500

    Remove unnecessary echo

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,7 +6,6 @@ use li3_memoize\extensions\Memoize;
 /**
  * Filters the creation of helpers/models and passes them into Memoize::instance
  */
-echo 'hello world';
 Filters::apply('lithium\core\Libraries', 'instance', function($self, $params, $chain) {
 	$object = $chain->next($self, $params, $chain);
 
