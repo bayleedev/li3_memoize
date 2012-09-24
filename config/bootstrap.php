96a433aa00b894df00275eaabc004187d9e99fa8
Blaine Schmeisser <BlaineSch@gmail.com>
2012-09-24 00:16:08 -0500

    Filter by models as well

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,10 +6,19 @@ use li3_memoize\extensions\Memoize;
 /**
  * Filters the creation of helpers and passes them into Memoize::instance
  */
+echo 'hello world';
 Filters::apply('lithium\core\Libraries', 'instance', function($self, $params, $chain) {
 	$object = $chain->next($self, $params, $chain);
+
+	// Helper
 	if($params['type'] == 'helper') {
 		$object = Memoize::instance($object);
 	}
+
+	// Model
+	if($params['name'] == 'lithium\data\collection\RecordSet') {
+		$object = Memoize::instance($object, $params['options']['model']);
+	}
+
 	return $object;
 });
