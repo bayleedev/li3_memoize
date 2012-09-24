599e8dc0fd36bc0336e83a036abfb9ee735b8567
Blaine Schmeisser <blaine.schmeisser@vitals.com>
2012-09-24 09:28:20 -0500

    Change helper to '_object' since we can now filter models as well.
    The underscore makes it more difficult for overlap.

--- extensions/adapter/storage/cache/DocumentMemoizerProxy.php
+++ extensions/adapter/storage/cache/DocumentMemoizerProxy.php
@@ -13,62 +13,62 @@ class DocumentMemoizerProxy extends MemoizerProxy implements \Iterator, \ArrayAc
 	 * Iterator::current — Return the current element
 	 */
 	public function current() {
-		return $this->helper->current();
+		return $this->_object->current();
 	}
 
 	/**
 	 * Iterator::key — Return the key of the current element
 	 */
 	public function key() {
-		return $this->helper->key();
+		return $this->_object->key();
 	}
 
 	/**
 	 * Iterator::next — Move forward to next element
 	 */
 	public function next() {
-		return $this->helper->next();
+		return $this->_object->next();
 	}
 
 	/**
 	 * Iterator::rewind — Rewind the Iterator to the first element
 	 */
 	public function rewind() {
-		return $this->helper->rewind();
+		return $this->_object->rewind();
 	}
 
 	/**
 	 * Iterator::valid — Checks if current position is valid
 	 */
 	public function valid() {
-		return $this->helper->valid();
+		return $this->_object->valid();
 	}
 
 	/**
 	 * ArrayAccess::offsetExists — Whether a offset exists
 	 */
 	public function offsetExists($key) {
-		return $this->helper->offsetExists($key);
+		return $this->_object->offsetExists($key);
 	}
 
 	/**
 	 * ArrayAccess::offsetGet — Offset to retrieve
 	 */
 	public function offsetGet($key) {
-		return $this->helper->offsetGet($key);
+		return $this->_object->offsetGet($key);
 	}
 
 	/**
 	 * ArrayAccess::offsetSet — Offset to set
 	 */
 	public function offsetSet($key, $value) {
-		return $this->helper->offsetSet($key, $value);
+		return $this->_object->offsetSet($key, $value);
 	}
 
 	/**
 	 * ArrayAccess::offsetUnset — Offset to unset
 	 */
 	public function offsetUnset($key) {
-		return $this->helper->offsetUnset($key);
+		return $this->_object->offsetUnset($key);
 	}
 }
